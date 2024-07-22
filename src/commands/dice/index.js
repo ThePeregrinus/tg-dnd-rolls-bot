9d193e450c187dca12f732a5c0fcfc982e43936a
Peregrinus <zakharov.mark22@gmail.com>
2024-07-22 08:29:22 +0300

    dice command

---
+++ src/commands/dice/index.js
@@ -0,0 +1,40 @@
+export const menu_handler = (bot) => {
+        const roll = (n) => {
+            return `You throw ${n}-side dice. The die came up as a ${Math.floor(Math.random() * n) + 1}`
+        }
+        
+        bot.command('d4', (ctx)=>{
+            ctx.reply(roll(4))
+        })
+        bot.command('d6', (ctx)=>{
+            ctx.reply(roll(6))
+        })
+        bot.command('d8', (ctx)=>{
+            ctx.reply(roll(8))
+        })
+        bot.command('d10',(ctx)=>{
+            ctx.reply(roll(10))
+        })
+        bot.command('d12', (ctx)=>{
+            ctx.reply(roll(12))
+        })
+        bot.command('d20', (ctx)=>{
+            ctx.reply(roll(20))
+        })
+        bot.catch((err)=>{
+            const ctx = err.ctx;
+            console.error(`Error while handling update ${ctx.update.update_id}:`);
+            ctx.reply('Error request. Try again')
+
+            if (e instanceof GrammyError) {
+                console.error("Error in request:", e.description);
+              } else if (e instanceof HttpError) {
+                console.error("Could not contact Telegram:", e);
+              } else {
+                console.error("Unknown error:", e);
+              }
+        })
+} 
+        
+
+
--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@ import { Bot } from "grammy";
 import 'dotenv/config';
 
 import { init_menu } from "./menu/index.js";
+import { menu_handler } from "./commands/dice/index.js";
 
 const token = process.env.TG_TOKEN;
 
 const bot = new Bot(token);
 
 init_menu(bot)
-
+menu_handler(bot)
 
 bot.start();
