a1e9061273cc6637b96d81638577bc95214b3924
Peregrinus <zakharov.mark22@gmail.com>
2024-07-22 06:13:33 +0300

    menu

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import { Bot } from "grammy";
 import 'dotenv/config';
+
+import { init_menu } from "./menu/index.js";
+
 const token = process.env.TG_TOKEN;
 
 const bot = new Bot(token);
 
-bot.on("message", (ctx) => ctx.reply('Hello, world!'));
+init_menu(bot)
+
 
 bot.start();
---
+++ src/menu/index.js
@@ -0,0 +1,11 @@
+export const init_menu = (bot) => {
+     bot.api.setMyCommands([
+    { command: "start", description: "Start bot" },
+    { command: "help", description: "Get instuctions" },
+    { command: "d4", description: "Roll a D4 dice" },
+    { command: "d6", description: "Roll a D6 dice" },
+    { command: "d8", description: "Roll a D8 dice" },
+    { command: "d10", description: "Roll a D10 dice" },
+    { command: "d12", description: "Roll a D12 dice" },
+    { command: "d20", description: "Roll a D20 dice" },
+    ])};
